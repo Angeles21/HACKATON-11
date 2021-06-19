2fdb9da9b9642ac439a6778589c4e4a4a6c28ff1
Angeles21 <pangeles218@gmail.com>
2021-06-19 11:58:50 -0500

    first changes

---
+++ app.js
@@ -0,0 +1,57 @@
+/* VARIABLES */
+const father = document.getElementById("father");
+
+/* FIRST ARRAY COLLECTION */
+
+let items = [];
+
+father.addEventListener("click", (e) => {
+	if (e.target.classList.contains("shopping")) {
+		// INFORMATION //
+		let title =
+			e.target.parentElement.parentElement.firstElementChild.nextElementSibling
+				.textContent;
+		let price =
+			e.target.parentElement.parentElement.firstElementChild.nextElementSibling
+				.nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling
+				.firstElementChild.textContent;
+		let img = e.target.parentElement.parentElement.firstElementChild;
+
+		// NEW OBJECT //
+
+		let selectedItems = {img, title, price: parseInt(price)};
+		console.log(selectedItems);
+
+		// ADDITION OF ALL PRICE TAGS //
+
+		items = [...items, selectedItems];
+		let result = items.reduce((sum, item) => {
+			return (sum += item.price);
+		}, 0);
+		console.log(result);
+
+		// CHANGES VISIBLE ON HTML //
+		const messageItems = document.getElementById("messageItems");
+
+		let newMessageItems = document.createElement("li");
+		newMessageItems.textContent = {selectedItems};
+
+		messageItems.appendChild(newMessageItems);
+
+		///
+
+		let totalItems = document.getElementById("totalItems");
+
+		let newTotalItems = document.createElement("span");
+		newTotalItems.textContent = result;
+
+		totalItems.appendChild(newTotalItems);
+
+		/*COUNTER ON THE CART*/
+		counter.textContent = items.length;
+
+		/*DISABLE BUTTON TO AVOID X2 PURCHASE*/
+		let button = e.target;
+		button.setAttribute("disabled", "");
+	}
+});
